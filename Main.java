b612f32076d264d06434a7f0d699202b82d1a8a6
JiayiH74 <jiayih12@nycstudents.net>
2024-10-09 23:32:27 +0000

    Update scoreboard

--- Main.java
+++ Main.java
@@ -1,1 +1,43 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        String info;
+        Scoreboard game = new Scoreboard("Red","Blue");
+        //test cases
+        info = game.getScore();     //Start game
+        System.out.println(info);
 
+        game.recordPlay(1);     //Red earns point
+        info = game.getScore();
+        System.out.println(info);
+
+        game.recordPlay(0);     //Red fails, switch team
+        info = game.getScore();
+        System.out.println(info);
+
+        game.recordPlay(3);     //Blue earns 3 points
+        info = game.getScore();
+        System.out.println(info);
+        
+        game.recordPlay(1);     //Blue earns 1 point
+        info = game.getScore();
+        System.out.println(info);
+
+        game.recordPlay(0);     //Blue fails, switches to red
+        info = game.getScore();
+        System.out.println(info);
+
+        game.recordPlay(0);     //Red fails, switches to blue
+        info = game.getScore();
+        System.out.println(info);
+
+        game.recordPlay(4);     //Blue scores 4 points
+        info = game.getScore();
+        System.out.println(info);
+
+        game.recordPlay(0);     //Switches to red
+        info = game.getScore();
+        System.out.println(info);
+    }
+}
--- Scoreboard.java
+++ Scoreboard.java
@@ -1,1 +1,37 @@
-
+public class Scoreboard
+{
+    private String teamOne;
+    private String teamTwo;
+    private int teamOneScore;
+    private int teamTwoScore;
+    private String activeTeam;
+    //Stores values
+    public Scoreboard(String one, String two)
+    {
+        teamOne = one;
+        teamTwo = two;
+        activeTeam = teamOne;
+    }
+    //Returns the scores as a string
+    public String getScore()
+    {
+        return teamOneScore + "-" + teamTwoScore + "-" + activeTeam;
+    }
+    
+    public void recordPlay(int score)
+    {
+        if(teamOne.equals(activeTeam))
+        {
+            teamOneScore += score;
+        }
+        if(teamTwo.equals(activeTeam))
+        {
+            teamTwoScore += score;
+        }
+        if (score == 0)
+        {
+            if(activeTeam.equals(teamOne)) activeTeam = teamTwo;
+            else if(activeTeam.equals(teamTwo)) activeTeam = teamOne;
+        }
+    }
+}
